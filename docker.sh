8a517c5592cd93caa79807b011f74c01ab415580
ngel123 <72899192+ngel123@users.noreply.github.com>
2021-02-13 22:16:12 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,42 +1,84 @@
-ZIPNAME=R-$(date +"%S-%F")
-git clone https://github.com/Thagoo/AnyKernel3 -b ts-proton-rova --depth 1 /tmp/AnyKernel3
+#!/usr/bin/env bash
+#
+# Copyright (C) 2018-2019 Rama Bondan Prakoso (rama982)
+#
+# Docker Kernel Build Script
 
-mkdir -p out
+# TELEGRAM START
+git clone --depth=1 https://github.com/fabianonline/telegram.sh telegram
 
-export KBUILD_BUILD_USER=Thago
-export KBUILD_BUILD_HOST=Drone
+TELEGRAM=telegram/telegram
+
+tg_channelcast() {
+  "${TELEGRAM}" -f "$(echo "$ZIP_DIR"/*.zip)" \
+  -t $TELEGRAM_TOKEN \
+  -c $CHAT_ID -H \
+      "$(
+          for POST in "${@}"; do
+              echo "${POST}"
+          done
+      )"
+}
+# TELEGRAM END
+
+# Main environtment
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+KERNEL_DIR=$(pwd)
+PARENT_DIR="$(dirname "$KERNEL_DIR")"
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+DTBO_IMG=$KERNEL_DIR/out/arch/arm64/boot/dtbo.img
+NAME="Realme 5, 5s, 5i / r5x"
+
+git submodule update --init --recursive
+git clone https://github.com/dodyirawan85/AnyKernel3.git 
+git clone https://github.com/silont-project/silont-clang.git --depth=1
+
+# Build kernel
+export TZ="Asia/Jakarta"
+export PATH="$PWD/silont-clang/bin:$PATH"
+export KBUILD_COMPILER_STRING="$PWD/silont-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+export DEVICE="r5x"
+export ARCH=arm64
+export KBUILD_BUILD_USER="huril"
+KBUILD_BUILD_TIMESTAMP=$(date)
+
+build_kernel () {
+    make -j$(nproc --all) O=out \
+        ARCH=arm64 \
+        CC=clang \
+        AR=llvm-ar OBJDUMP=llvm-objdump \
+        STRIP=llvm-strip \
+        OBJCOPY=llvm-objcopy \
+        CROSS_COMPILE=aarch64-linux-gnu- \
+        CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+}
+
 make O=out ARCH=arm64 ab_defconfig
-
-curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
-        -d chat_id="$CID" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="build started"
-
-PATH="/tmp/proton/bin:$PATH" \
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC=clang \
-                      CROSS_COMPILE=aarch64-linux-gnu- \
-                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-                      AR=llvm-ar \
-                      NM=llvm-nm
-
-if ! [ -a "out/arch/arm64/boot/Image.gz-dtb" ]; then
-	curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
-        -d chat_id="$CID" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="Build stopped Compilation Error"
-           exit 1
+build_kernel
+if ! [ -a $KERN_IMG ]; then
+    tg_channelcast "<b>BuildCI report status:</b> There are build running but its error, please fix and remove this message!"
+    exit 1
 fi
-cp out/arch/arm64/boot/Image.gz-dtb /tmp/AnyKernel3
-cd /tmp/AnyKernel3
-zip -r9 ${ZIPNAME}.zip *
 
-curl -F document=@$ZIPNAME.zip "https://api.telegram.org/bot$TOKEN/sendDocument" \
-        -F chat_id=$CID\
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html"  \
-	-F caption="#triton #storm #4.9 #Rova
-Compiler- Proton-Clang-v12 follow @tboxxx for more updates"
+# Make zip installer
+
+# ENV
+ZIP_DIR=$KERNEL_DIR/AnyKernel3
+
+# Modify kernel name in anykernel
+sed -i 's/ExampleKernel by osm0sis @ xda-developer/'$KERNAME' by dodyirawan85 @ github.com/g' $ZIP_DIR/anykernel.sh
+
+# Make zip
+make -C $ZIP_DIR clean
+cp $KERN_IMG $ZIP_DIR
+cp $DTBO_IMG $ZIP_DIR
+make -C $ZIP_DIR normal
+
+KERNEL=$(cat out/.config | grep Linux/arm64 | cut -d " " -f3)
+FILEPATH=$(echo "$ZIP_DIR"/*.zip)
+HASH=$(git log --pretty=format:'%h' -1)
+COMMIT=$(git log --pretty=format:'%h: %s' -1)
+tg_channelcast "<b>Latest commit:</b> <a href='https://github.com/dodyirawan85/android_kernel_realme_trinket/commits/$HASH'>$COMMIT</a>" \
+               "<b>Kernel:</b> $KERNEL" \
+               "<b>sha1sum:</b> <pre>$(sha1sum "$FILEPATH" | awk '{ print $1 }')</pre>" \
+               "<b>Date:</b> $KBUILD_BUILD_TIMESTAMP"
